128ac5676124e6dab313b90437fcf558282ab97a
Shikha Singh <150659967+SiyaShikha@users.noreply.github.com>
2024-11-24 00:42:48 +0530

    Update rock_paper_scissors_game.js

--- rock_paper_scissors_game.js
+++ rock_paper_scissors_game.js
@@ -1,6 +1,10 @@
-// 0 for Rock
-// 2 for Scissors
-// 5 for paper
+const ROCK = 0;
+const PAPER = 5;
+const SCISSORS = 2;
+const tie = decorateMessage("🤝Its a tie!");
+const playerWin = decorateMessage("🏆Congratulations! You won.");
+const compWin = decorateMessage("Ops! You Lost.🤞Better Luck Next Time.");
+const invalid = decorateMessage("❌ Invalid Choice.");
 
 function repeat(string, nTimes) {
   if (nTimes <= 0) {
@@ -10,68 +14,80 @@ function repeat(string, nTimes) {
   return string + repeat(string, nTimes - 1);
 }
 
-function getBox(string) {
+function decorateMessage(string) {
   const line = repeat("―", string.length);
   return line + "\n" + string + "\n" + line;
 }
-const welcomeMessage = getBox("🪨🗒 ✂️ Welcome to ROCK PAPER SCISSORS ✂️ 🗒 🪨");
-const rock = "\n✦ 0️ is for ROCK✊";
-const paper = "\n✦ 5️ is for PAPER🖐";
-const scissors = "\n✦ 2️ is for SCISSORS✌️\n";
-
-console.log(welcomeMessage + "\nINSTRUCTIONS -" + rock + paper + scissors);
-
-const tie = getBox("🤝Its a tie!");
-const playerWin = getBox("🏆Congratulations! You won.");
-const compWin = getBox("Ops! You Lost.🤞Better Luck Next Time.");
-const invalid = getBox("❌ Invalid Choice.");
 
 function decideWinner(playerChoice, compChoice) {
   if (playerChoice === compChoice) {
     return tie;
   }
-
-  if (!(playerChoice === 0 || playerChoice === 5 || playerChoice === 2)) {
-    return invalid;
+  if (playerChoice === ROCK) {
+    return compChoice === PAPER ? compWin : playerWin;
   }
-
-  if (playerChoice === 0) {
-    return compChoice === 5 ? compWin : playerWin;
+  if (playerChoice === PAPER) {
+    return compChoice === SCISSORS ? compWin : playerWin;
   }
-
-  if (playerChoice === 5) {
-    return compChoice === 2 ? compWin : playerWin;
-  }
-
-  if (playerChoice === 2) {
-    return compChoice === 0 ? compWin : playerWin;
+  if (playerChoice === SCISSORS) {
+    return compChoice === ROCK ? compWin : playerWin;
   }
 }
 
-function startPlaying() {
-  const playerChoice = +prompt("Enter Your Choice (0,5,2) : ");
-  let compChoice = Math.floor(Math.random() * 10) % 3;
+function showWelcomeMessage() {
+  const welcomeMessage = decorateMessage("🪨🗒 ✂️ Welcome to ROCK PAPER SCISSORS ✂️ 🗒 🪨");
+  const rock = "\n🔹 0️ is for ROCK✊";
+  const paper = "\n🔹 5️ is for PAPER🖐";
+  const scissors = "\n🔹 2️ is for SCISSORS✌️\n";
 
-  if (compChoice === 1) {
-    compChoice = 5;
+  console.log(welcomeMessage + "\n👉🏻 INSTRUCTIONS -" + rock + paper + scissors);
+}
+
+function userWantsToPlayAgain() {
+  return confirm("\nDo you want to play new game?");
+}
+
+function showGoodByeMessage() {
+  console.log(decorateMessage("🙋🏻‍♀️GoodBye!"));
+}
+
+function readPlayerChoice() {
+  const choice = +prompt("🔴Enter Your Choice (0,5,2) : ");
+
+  if (choice !== ROCK && choice !== PAPER && choice !== SCISSORS) {
+    console.log(invalid);
+    return readPlayerChoice();
   }
 
-  console.log("\nPlayer👩🏻  : " + playerChoice);
-  console.log("Computer : " + compChoice);
+  return choice;
+}
 
+function generateComputerChoice() {
+  let choice = Math.floor(Math.random() * 10) % 3;
+  if (choice === 1) {
+    choice = PAPER;
+  }
+  return choice;
+}
+
+function showPlayerAndComputerChoice(playerChoice, compChoice) {
+  console.log("\n👩🏻Player    : " + playerChoice);
+  console.log("🌐Computer  : " + compChoice);
+}
+
+function startGame() {
+  showWelcomeMessage();
+  const playerChoice = readPlayerChoice();
+  const compChoice = generateComputerChoice();
+
+  showPlayerAndComputerChoice(playerChoice, compChoice);
   console.log(decideWinner(playerChoice, compChoice));
 
-  if (confirm("\nDo you want to play new game?")) {
-    startPlaying();
-  } else {
-    console.log(getBox("🙋🏻‍♀️GoodBye!"));
+  if (userWantsToPlayAgain()) {
+    return startGame();
   }
+
+  showGoodByeMessage();
 }
 
-const start = confirm("Do you want to start the game?");
-
-if (start) {
-  startPlaying();
-} else {
-  console.log(getBox("Hope You play next time!"));
-}
+startGame();
